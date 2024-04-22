d113bafe3cd2f847e2c9ca63ba2b1d7b68afd849
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-22 12:36:31 -0400

    added funtction to start the app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { prompt } = require('inquirer');
 const mysql = require('mysql');
 
-// Create a connection to the MySQL database
+// create a connection to the MySQL database
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -9,3 +9,9 @@ const connection = mysql.createConnection({
     password: 'your_password',
     database: 'your_database'
   });
+
+// start the application
+function startApp() {
+    console.log('Welcome to Employee Management System!');
+    promptUser();
+  }
