6179d980d6aa2192dd927626fce9d443815e6157
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-23 11:54:19 -0400

    added more functions to the prompt section in index.js

--- index.js
+++ index.js
@@ -10,68 +10,71 @@ const connection = mysql.createConnection({
     user: 'your_username',
     password: 'your_password',
     database: 'your_database'
-  });
+});
 
 // start the application
 function startApp() {
     console.log('Welcome to Employee Management System!');
     promptUser();
-  }
+}
 
-  function init() {
+function init() {
     const logoText = logo({ name: "Employee Manager" }).render();
-  
+
     console.log(logoText);
-  }
+}
 
 // function to prompt the user with options
 function promptUser() {
     inquirer.prompt({
-      type: 'list',
-      name: 'option',
-      message: 'What would you like to do?',
-      choices: [
-        'View all departments',
-        'View all roles',
-        'View all employees',
-        'View all employees by Department',
-        'Add a department',
-        'Add a role',
-        'Add an employee',
-        'Update an employee role',
-        'Exit'
-      ]
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'View all employees by Department',
+            'View all employees by Manager',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
     }).then(answer => {
-      switch (answer.option) {
-        case 'View all departments':
-          viewDepartments();
-          break;
-        case 'View all roles':
-          viewRoles();
-          break;
-        case 'View all employees':
-          viewEmployees();
-          break;
-        case 'View all employees by Dapartment':
-          viewEmployeesByDepartment();
-          break;
-        case 'Add a department':
-          addDepartment();
-          break;
-        case 'Add a role':
-          addRole();
-          break;
-        case 'Add an employee':
-          addEmployee();
-          break;
-        case 'Update an employee role':
-          updateEmployeeRole();
-          break;
-        case 'Exit':
-          console.log('Goodbye!');
-          connection.end();
-          break;
-      }
+        switch (answer.option) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View all employees by Dapartment':
+                viewEmployeesByDepartment();
+                break;
+            case 'View all employees by Manager':
+                viewEmployeesByManager();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                console.log('Goodbye!');
+                connection.end();
+                break;
+        }
     });
-  }
-  
+}
