13de32832a4876d34d74b09925b59239647cc533
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-04-23 23:57:56 -0400

    added function to add a role

--- index.js
+++ index.js
@@ -270,3 +270,31 @@ function addDepartment() {
       });
     });
   }
+
+
+// function to add a role
+  function addRole() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of the role:'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the salary for the role:'
+      },
+      {
+        type: 'input',
+        name: 'department_id',
+        message: 'Enter the department ID for the role:'
+      }
+    ]).then(answers => {
+      connection.query('INSERT INTO roles SET ?', answers, (err, res) => {
+        if (err) throw err;
+        console.log('Role added successfully!');
+        promptUser();
+      });
+    });
+  }
